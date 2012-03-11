644be11960eeafa819465fa15475df9b711e8472
lamb <lamb52gao@gmail.com>
2012-03-11 18:04:14 +0800

    Combo import not used

--- org.nodejs.ide.eclipse/src/org/nodejs/ide/eclipse/launch/LaunchConfigurationMainTab.java
+++ org.nodejs.ide.eclipse/src/org/nodejs/ide/eclipse/launch/LaunchConfigurationMainTab.java
@@ -25,7 +25,6 @@ import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Text;
