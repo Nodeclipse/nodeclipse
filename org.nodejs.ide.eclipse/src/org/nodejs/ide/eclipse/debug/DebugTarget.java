3d0e6613ba6e80ca04357ded96664e071cf61c26
oncereply <oncereply@gmail.com>
2012-03-14 16:23:04 +0800

    LogUtil

--- org.nodejs.ide.eclipse/src/org/nodejs/ide/eclipse/debug/DebugTarget.java
+++ org.nodejs.ide.eclipse/src/org/nodejs/ide/eclipse/debug/DebugTarget.java
@@ -12,177 +12,179 @@ import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.core.model.IStreamsProxy;
 import org.eclipse.debug.core.model.IThread;
 import org.nodejs.ide.eclipse.Constants;
+import org.nodejs.ide.eclipse.utils.LogUtil;
 
 public class DebugTarget implements IDebugTarget, IDebugElement {
 
-    private ILaunch launch;
-    private IProcess process;
-    private Process p;
+	private ILaunch launch;
+	private IProcess process;
+	private Process p;
 
-    public DebugTarget(ILaunch launch, IProcess process, Process p) {
-        this.launch = launch;
-        this.process = process;
-        this.p = p;
-    }
+	public DebugTarget(ILaunch launch, IProcess process, Process p) {
+		this.launch = launch;
+		this.process = process;
+		this.p = p;
+	}
 
-    @SuppressWarnings("rawtypes")
-    @Override
-    public Object getAdapter(Class adapter) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Object getAdapter(Class adapter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public boolean canTerminate() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean canTerminate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public boolean isTerminated() {
-        return process.isTerminated();
-    }
+	@Override
+	public boolean isTerminated() {
+		return process.isTerminated();
+	}
 
-    @Override
-    public void terminate() throws DebugException {
-        IStreamsProxy streamsProxy = process.getStreamsProxy();
-        try {
-            if (!process.isTerminated()) {
-                streamsProxy.write(Constants.QUIT + Constants.EOL);
-                // wait for subprocess exit
-                while (true) {
-                    if (p.waitFor() == 0)
-                        break;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void terminate() throws DebugException {
+		IStreamsProxy streamsProxy = process.getStreamsProxy();
+		try {
+			if (!isTerminated()) {
+				streamsProxy.write(Constants.QUIT + Constants.EOL);
+				// wait for subprocess exit
+				while (true) {
+					if (p.waitFor() == 0)
+						break;
+				}
+			}
+		} catch (IOException e) {
+			LogUtil.error(e);
+		} catch (InterruptedException e) {
+			LogUtil.error(e);
+		}
+	}
 
-    @Override
-    public boolean canResume() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean canResume() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public boolean canSuspend() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean canSuspend() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public boolean isSuspended() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean isSuspended() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public void resume() throws DebugException {
-        // TODO Auto-generated method stub
+	@Override
+	public void resume() throws DebugException {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void suspend() throws DebugException {
-        // TODO Auto-generated method stub
+	@Override
+	public void suspend() throws DebugException {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void breakpointAdded(IBreakpoint breakpoint) {
-        // TODO Auto-generated method stub
+	@Override
+	public void breakpointAdded(IBreakpoint breakpoint) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void breakpointRemoved(IBreakpoint breakpoint, IMarkerDelta delta) {
-        // TODO Auto-generated method stub
+	@Override
+	public void breakpointRemoved(IBreakpoint breakpoint, IMarkerDelta delta) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public void breakpointChanged(IBreakpoint breakpoint, IMarkerDelta delta) {
-        // TODO Auto-generated method stub
+	@Override
+	public void breakpointChanged(IBreakpoint breakpoint, IMarkerDelta delta) {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public boolean canDisconnect() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean canDisconnect() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public void disconnect() throws DebugException {
-        // TODO Auto-generated method stub
+	@Override
+	public void disconnect() throws DebugException {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    public boolean isDisconnected() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean isDisconnected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public boolean supportsStorageRetrieval() {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean supportsStorageRetrieval() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public IMemoryBlock getMemoryBlock(long startAddress, long length) throws DebugException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public IMemoryBlock getMemoryBlock(long startAddress, long length)
+			throws DebugException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public String getModelIdentifier() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public String getModelIdentifier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public IDebugTarget getDebugTarget() {
-        return this;
-    }
+	@Override
+	public IDebugTarget getDebugTarget() {
+		return this;
+	}
 
-    @Override
-    public ILaunch getLaunch() {
-        return launch;
-    }
+	@Override
+	public ILaunch getLaunch() {
+		return launch;
+	}
 
-    @Override
-    public IProcess getProcess() {
-        return process;
-    }
+	@Override
+	public IProcess getProcess() {
+		return process;
+	}
 
-    @Override
-    public IThread[] getThreads() throws DebugException {
-        // TODO Auto-generated method stub
-        return new IThread[] {};
-    }
+	@Override
+	public IThread[] getThreads() throws DebugException {
+		// TODO Auto-generated method stub
+		return new IThread[] {};
+	}
 
-    @Override
-    public boolean hasThreads() throws DebugException {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean hasThreads() throws DebugException {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-    @Override
-    public String getName() throws DebugException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	@Override
+	public String getName() throws DebugException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-    @Override
-    public boolean supportsBreakpoint(IBreakpoint breakpoint) {
-        // TODO Auto-generated method stub
-        return false;
-    }
+	@Override
+	public boolean supportsBreakpoint(IBreakpoint breakpoint) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
