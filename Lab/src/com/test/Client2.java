8bdc157587fb758fe3afe616239f482cffae89af
lamb <lamb52gao@gmail.com>
2012-03-21 00:23:12 +0800

    Client2

--- Lab/src/com/test/Client2.java
+++ Lab/src/com/test/Client2.java
@@ -5,17 +5,18 @@ import java.net.*;
 
 public class Client2 {
     Socket socket;
-    PrintWriter os;
-    BufferedReader is;
+    OutputStream os;
+    InputStream is;
 
     Client2() {
         try {
             // 向本机的5859端口发出客户请求
             socket = new Socket("127.0.0.1", 5859);
+            socket.setKeepAlive(true);
             // 由Socket对象得到输出流，并构造PrintWriter对象
-            os = new PrintWriter(socket.getOutputStream());
+            os = socket.getOutputStream();
             // 由Socket对象得到输入流，并构造相应的BufferedReader对象
-            is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            is =socket.getInputStream();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -23,7 +24,7 @@ public class Client2 {
         }
     }
 
-    public void close(Socket socket, PrintWriter os, BufferedReader is) {
+    public void close() {
         try {
             // 关闭Socket输出流
             os.close();
@@ -36,31 +37,36 @@ public class Client2 {
         }
     }
 
-    public static void main(String args[]) throws IOException, CloneNotSupportedException {
+    public static void main(String args[]) throws Exception {
 
         // D:\Workspace\runtime-EclipseApplication\test\src
         // node --debug-brk=5859 index.js
         Client2 client = new Client2();
-        System.out.println(client.send("test"));
-        client.clone();
+        System.out.println(client.send("{\"seq\":1,\"type\":\"request\",\"command\":\"version\"}"));
+        Thread.sleep(20000);
+        client.close();
     }
 
     private String send(String commond) throws IOException {
-        os.println(commond);
-        // 将从系统标准输入读入的字符串输出到Server
-        os.flush();
-        // 刷新输出流，使Server马上收到该字符串
-        System.out.println("Client:" + commond);
-        // 在系统标准输出上打印读入的字符串
-        StringBuffer sb = new StringBuffer("");
-        String line = null;
-        while ((line = is.readLine()) != null) {
-            sb.append(line + "\n");
-            System.out.print(line + "\n");
+        synchronized (socket) {
+            os.write(commond.getBytes());
+            // 将从系统标准输入读入的字符串输出到Server
+            os.flush();
+            // 刷新输出流，使Server马上收到该字符串
+            System.out.println("Client:" + commond);
+            // 在系统标准输出上打印读入的字符串
+            StringBuffer sb = new StringBuffer("");
+            int availableCount;
+            while((availableCount=is.available()) > 0){  
+                byte[] requestbuffer = new byte[availableCount];  
+                is.read(requestbuffer);
+                sb.append(new String(requestbuffer, "UTF-8"));  
+            }   
+            System.out.println("~~~~");
+            String response = sb.toString();
+            // 从Server读入一字符串，并打印到标准输出上
+            System.out.println("Server: return end" + response);
+            return response;
         }
-        String response = sb.toString();
-        // 从Server读入一字符串，并打印到标准输出上
-        System.out.println("Server:" + response);
-        return response;
     }
 }
