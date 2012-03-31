6823c16f5c8c395d2d91bce744b57e067d59d48c
lamb <lamb52gao@gmail.com>
2012-04-01 01:16:45 +0800

    代码自动提示功能

--- org.nodeclipse.ui/src/org/nodeclipse/ui/editors/NodejsSourceViewerConfiguration.java
+++ org.nodeclipse.ui/src/org/nodeclipse/ui/editors/NodejsSourceViewerConfiguration.java
@@ -2,6 +2,8 @@ package org.nodeclipse.ui.editors;
 
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.jface.text.contentassist.ContentAssistant;
+import org.eclipse.jface.text.contentassist.IContentAssistant;
 import org.eclipse.jface.text.presentation.IPresentationReconciler;
 import org.eclipse.jface.text.presentation.PresentationReconciler;
 import org.eclipse.jface.text.rules.BufferedRuleBasedScanner;
@@ -43,6 +45,20 @@ public class NodejsSourceViewerConfiguration extends SourceViewerConfiguration {
     @Override
     public String[] getConfiguredContentTypes(ISourceViewer sourceViewer) {
         return CONTENT_TYPES;
+    }
+
+    @Override
+    public IContentAssistant getContentAssistant(ISourceViewer sourceViewer) {
+        // TODO 代码提示
+        // 创建内容助手对象
+        ContentAssistant contentAssistant = new ContentAssistant();
+        // 设置提示的内容
+        contentAssistant.setContentAssistProcessor(new ObjectContentAssistant(), IDocument.DEFAULT_CONTENT_TYPE);
+        // 设置自动激活提示
+        contentAssistant.enableAutoActivation(true);
+        // 设置自动激活提示的时间为500毫秒
+        contentAssistant.setAutoActivationDelay(500);
+        return contentAssistant;
     }
 
 }
