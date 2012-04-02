d46958403aeb9ccc65391697d355395ebac3216e
oncereply <oncereply@gmail.com>
2012-04-02 21:20:11 +0800

    解决update site build时一些错误log

--- org.nodeclipse.ui/src/org/nodeclipse/ui/contentassist/NodejsContentAssistant.java
+++ org.nodeclipse.ui/src/org/nodeclipse/ui/contentassist/NodejsContentAssistant.java
@@ -14,7 +14,12 @@ import org.eclipse.jface.text.contentassist.IContextInformationValidator;
 import org.nodeclipse.ui.util.Constants;
 
 public class NodejsContentAssistant implements IContentAssistProcessor {
-    // 接口中的方法，获得内容的提示数组
+
+    private static final String[] JS_SYNTAX_BUILDIB_OBJECT = Constants.JS_SYNTAX_BUILDIB_OBJECT;
+
+    /**
+     * 接口中的方法，获得内容的提示数组
+     */
     @Override
     public ICompletionProposal[] computeCompletionProposals(ITextViewer viewer, int offset) {
         IDocument doc = viewer.getDocument();
@@ -29,7 +34,9 @@ public class NodejsContentAssistant implements IContentAssistProcessor {
         return null;
     }
 
-    // 接口中的方法，设置何时激活提示，这里当输入.时激活内容提示
+    /**
+     * 接口中的方法，设置何时激活提示，这里当输入.时激活内容提示
+     */
     @Override
     public char[] getCompletionProposalAutoActivationCharacters() {
         // TODO 首选项支持
@@ -54,7 +61,13 @@ public class NodejsContentAssistant implements IContentAssistProcessor {
         return null;
     }
 
-    // 获得获得提示时，之前输入的字符
+    /**
+     * 获得获得提示时，之前输入的字符
+     * 
+     * @param doc
+     * @param offset
+     * @return
+     */
     public String getObjectName(IDocument doc, int offset) {
         // offset为当前光标所在位置，也就是偏移量
         StringBuffer buf = new StringBuffer();
@@ -63,26 +76,36 @@ public class NodejsContentAssistant implements IContentAssistProcessor {
         // 然后将字符串反转
         while (true) {
             try {
-                char c = doc.getChar(--offset);// 获得前一个字符
-                if (Character.isWhitespace(c))// 如果为空格，跳出
+                // 获得前一个字符
+                char charOffset = doc.getChar(--offset);
+                // 如果为空格，跳出
+                if (Character.isWhitespace(charOffset))
                     break;
-                if (c == '.')// 如果为“.”则跳出
+                // 如果为“.”则跳出
+                if (charOffset == '.')
                     break;
-                buf.append(c);
+                buf.append(charOffset);
             } catch (BadLocationException e) {
                 break;
             }
         }
-        return buf.reverse().toString();// 最后将字符反转
+        // 最后将字符反转
+        return buf.reverse().toString();
     }
 
-    // 设置内容提示的中的内容
+    /**
+     * 设置内容提示的中的内容
+     * 
+     * @param objName
+     * @param offset
+     * @return
+     */
     public List<CompletionProposal> computObject(String objName, int offset) {
         // objName首先看objName是否为内置的对象
         List<CompletionProposal> list = new ArrayList<CompletionProposal>();
         boolean bFind = false;
-        for (int i = 0; i < Constants.JS_SYNTAX_BUILDIB_OBJECT.length; i++) {
-            String tempString = Constants.JS_SYNTAX_BUILDIB_OBJECT[i];
+        for (int i = 0; i < JS_SYNTAX_BUILDIB_OBJECT.length; i++) {
+            String tempString = JS_SYNTAX_BUILDIB_OBJECT[i];
             if (objName.equals(tempString)) {
                 bFind = true;
                 break;
@@ -90,8 +113,8 @@ public class NodejsContentAssistant implements IContentAssistProcessor {
         }
         // 如果是内置对象，则将所有的对象的字符添加到内容提示列表中
         if (bFind) {
-            for (int i = 0; i < Constants.JS_SYNTAX_BUILDIB_OBJECT.length; i++) {
-                String insert = objName + "." + Constants.JS_SYNTAX_BUILDIB_OBJECT[i];
+            for (int i = 0; i < JS_SYNTAX_BUILDIB_OBJECT.length; i++) {
+                String insert = objName + "." + JS_SYNTAX_BUILDIB_OBJECT[i];
                 // CompletionProposal对象
                 CompletionProposal proposal = new CompletionProposal(insert, offset - objName.length() - 1, objName.length() + 1,
                         insert.length() + 1, null, null, null, "aaa");
--- org.nodeclipse.ui/src/org/nodeclipse/ui/util/Constants.java
+++ org.nodeclipse.ui/src/org/nodeclipse/ui/util/Constants.java
@@ -1,16 +1,14 @@
 package org.nodeclipse.ui.util;
 
 public class Constants {
-    // 语法使用的关键字
-    public static final String[] JS_SYNTAX_KEYWORD = { "abstract", "boolean", "break", "byte", "case", "catch", "char", "class", "const",
-            "continue", "default", "delete", "do", "double", "else", "extends", "false", "final", "finally", "float", "for", "function",
-            "goto", "if", "implements", "import", "short", "in", "instanceof", "int", "interface", "long", "native", "new", "null",
-            "package", "private", "protected", "public", "return", "short", "static", "super", "switch", "synchronized", "this", "throw",
-            "throws", "transient", "true", "try", "typeof", "var", "void", "while", "with", "script", "language" };
-    // 语法使用的内置对象
-    public static final String[] JS_SYNTAX_BUILDIB_OBJECT = { "Anchor", "anchors", "Applet", "applets", "Area", "Array", "Button",
-            "Checkbox", "Date", "document", "FileUpload", "Form", "forms", "Frame", "frames", "Hidden", "history", "Image", "images",
-            "Link", "write" };
+    /**
+     * 语法使用的关键字
+     */
+    public static final String[] JS_SYNTAX_KEYWORD = { "abstract", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "default", "delete", "do", "double", "else", "extends", "false", "final", "finally", "float", "for", "function", "goto", "if", "implements", "import", "short", "in", "instanceof", "int", "interface", "long", "native", "new", "null", "package", "private", "protected", "public", "return", "short", "static", "super", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "typeof", "var", "void", "while", "with", "script", "language" };
+    /**
+     * 语法使用的内置对象
+     */
+    public static final String[] JS_SYNTAX_BUILDIB_OBJECT = { "Anchor", "anchors", "Applet", "applets", "Area", "Array", "Button", "Checkbox", "Date", "document", "FileUpload", "Form", "forms", "Frame", "frames", "Hidden", "history", "Image", "images", "Link", "write" };
 
     public static void main(String[] args) {
         System.out.println(JS_SYNTAX_KEYWORD.length);
