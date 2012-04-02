7069ab1b6c5ece68fbb2c04c99148fc1e612aa01
oncereply <oncereply@gmail.com>
2012-04-03 00:11:42 +0800

    匹配双括号

--- org.nodeclipse.ui/src/org/nodeclipse/ui/editors/NodejsEditor.java
+++ org.nodeclipse.ui/src/org/nodeclipse/ui/editors/NodejsEditor.java
@@ -1,14 +1,20 @@
 package org.nodeclipse.ui.editors;
 
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.text.IDocumentExtension3;
+import org.eclipse.jface.text.source.DefaultCharacterPairMatcher;
 import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.texteditor.SourceViewerDecorationSupport;
 
 public class NodejsEditor extends TextEditor {
 
     public static final String EDITOR_ID = "org.nodeclipse.ui.editors.NodejsEditor";
-
     public static final String EDITOR_CONTEXT = EDITOR_ID + ".context";
-
     public static final String RULER_CONTEXT = EDITOR_ID + ".ruler";
+    public final static String EDITOR_MATCHING_BRACKETS = "matchingBrackets";
+    public final static String EDITOR_MATCHING_BRACKETS_COLOR = "matchingBracketsColor";
+    
+    private DefaultCharacterPairMatcher matcher;
 
     public NodejsEditor() {
         setSourceViewerConfiguration(new NodejsSourceViewerConfiguration());
@@ -22,5 +28,25 @@ public class NodejsEditor extends TextEditor {
         setRulerContextMenuId(RULER_CONTEXT);
         setDocumentProvider(new NodejsDocumentProvider());
     }
+
+    /**
+     * 匹配双括号
+     */
+	@Override
+	protected void configureSourceViewerDecorationSupport(
+			SourceViewerDecorationSupport support) {
+		super.configureSourceViewerDecorationSupport(support);
+
+	    char[] matchChars = {'(', ')', '[', ']', '{', '}'}; //which brackets to match
+	    matcher = new DefaultCharacterPairMatcher(matchChars, IDocumentExtension3.DEFAULT_PARTITIONING);
+	    support.setCharacterPairMatcher(matcher);
+	    support.setMatchingCharacterPainterPreferenceKeys(EDITOR_MATCHING_BRACKETS,
+	        EDITOR_MATCHING_BRACKETS_COLOR);
+
+	    //Enable bracket highlighting in the preference store
+	    IPreferenceStore store = getPreferenceStore();
+	    store.setDefault(EDITOR_MATCHING_BRACKETS, true);
+	    store.setDefault(EDITOR_MATCHING_BRACKETS_COLOR, "128,128,128");
+	}
 
 }
