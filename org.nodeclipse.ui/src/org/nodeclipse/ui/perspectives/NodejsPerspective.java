58ac15eb4cfd61aea9c53be9e1d9d9a0d9101690
lamb <lamb52gao@gmail.com>
2012-04-22 23:28:34 +0800

    fmt code

--- org.nodeclipse.ui/src/org/nodeclipse/ui/perspectives/NodejsPerspective.java
+++ org.nodeclipse.ui/src/org/nodeclipse/ui/perspectives/NodejsPerspective.java
@@ -15,70 +15,67 @@ import org.eclipse.jdt.ui.JavaUI;
  */
 public class NodejsPerspective implements IPerspectiveFactory {
 
-	public static final String ID = "org.nodeclipse.ui.perspectives.NodejsPerspective";
-	private IPageLayout factory;
+    public static final String ID = "org.nodeclipse.ui.perspectives.NodejsPerspective";
+    private IPageLayout factory;
 
-	public NodejsPerspective() {
-		super();
-	}
+    public NodejsPerspective() {
+        super();
+    }
 
-	public void createInitialLayout(IPageLayout factory) {
-		this.factory = factory;
-		addViews();
-		addActionSets();
-		addNewWizardShortcuts();
-		addPerspectiveShortcuts();
-		addViewShortcuts();
-	}
+    public void createInitialLayout(IPageLayout factory) {
+        this.factory = factory;
+        addViews();
+        addActionSets();
+        addNewWizardShortcuts();
+        addPerspectiveShortcuts();
+        addViewShortcuts();
+    }
 
-	private void addViews() {
-		// Creates the overall folder layout.
-		// Note that each new Folder uses a percentage of the remaining
-		// EditorArea.
+    private void addViews() {
+        // Creates the overall folder layout.
+        // Note that each new Folder uses a percentage of the remaining
+        // EditorArea.
 
-		IFolderLayout left = factory.createFolder("left", IPageLayout.LEFT,
-				0.25f, factory.getEditorArea());
-		left.addView(IPageLayout.ID_PROJECT_EXPLORER);
+        IFolderLayout left = factory.createFolder("left", IPageLayout.LEFT, 0.25f, factory.getEditorArea());
+        left.addView(IPageLayout.ID_PROJECT_EXPLORER);
 
-		IFolderLayout rightBottom = factory.createFolder("rightBottom", // NON-NLS-1
-				IPageLayout.BOTTOM, 0.75f, factory.getEditorArea());
-		rightBottom.addView(IPageLayout.ID_PROBLEM_VIEW);
-		rightBottom.addPlaceholder(IConsoleConstants.ID_CONSOLE_VIEW);
+        IFolderLayout rightBottom = factory.createFolder("rightBottom", IPageLayout.BOTTOM, 0.75f, factory.getEditorArea());// NON-NLS-1
+        rightBottom.addView(IPageLayout.ID_PROBLEM_VIEW);
+        rightBottom.addPlaceholder(IConsoleConstants.ID_CONSOLE_VIEW);
 
-		IFolderLayout rightTopRight = factory.createFolder("rightTopRight",
-				IPageLayout.RIGHT, 0.75f, factory.getEditorArea());
-		rightTopRight.addView(IPageLayout.ID_OUTLINE);
-		rightTopRight.addView(IPageLayout.ID_TASK_LIST);
-	}
+        IFolderLayout rightTopRight = factory.createFolder("rightTopRight", IPageLayout.RIGHT, 0.75f, factory.getEditorArea());
+        rightTopRight.addView(IPageLayout.ID_OUTLINE);
+        rightTopRight.addView(IPageLayout.ID_TASK_LIST);
+    }
 
-	private void addActionSets() {
-		factory.addActionSet("org.eclipse.debug.ui.launchActionSet"); // NON-NLS-1
-		factory.addActionSet("org.eclipse.debug.ui.debugActionSet"); // NON-NLS-1
-		factory.addActionSet("org.eclipse.debug.ui.profileActionSet"); // NON-NLS-1
-		factory.addActionSet("org.eclipse.team.ui.actionSet"); // NON-NLS-1
-		factory.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET); // NON-NLS-1
-	}
+    private void addActionSets() {
+        factory.addActionSet("org.eclipse.debug.ui.launchActionSet"); // NON-NLS-1
+        factory.addActionSet("org.eclipse.debug.ui.debugActionSet"); // NON-NLS-1
+        factory.addActionSet("org.eclipse.debug.ui.profileActionSet"); // NON-NLS-1
+        factory.addActionSet("org.eclipse.team.ui.actionSet"); // NON-NLS-1
+        factory.addActionSet(IPageLayout.ID_NAVIGATE_ACTION_SET); // NON-NLS-1
+    }
 
-	private void addPerspectiveShortcuts() {
-		factory.addPerspectiveShortcut("org.eclipse.team.ui.TeamSynchronizingPerspective"); // NON-NLS-1
-		factory.addPerspectiveShortcut("org.eclipse.ui.resourcePerspective"); // NON-NLS-1
-		factory.addPerspectiveShortcut(IDebugUIConstants.ID_DEBUG_PERSPECTIVE);
-	}
+    private void addPerspectiveShortcuts() {
+        factory.addPerspectiveShortcut("org.eclipse.team.ui.TeamSynchronizingPerspective"); // NON-NLS-1
+        factory.addPerspectiveShortcut("org.eclipse.ui.resourcePerspective"); // NON-NLS-1
+        factory.addPerspectiveShortcut(IDebugUIConstants.ID_DEBUG_PERSPECTIVE);
+    }
 
-	private void addNewWizardShortcuts() {
-		factory.addNewWizardShortcut("org.nodeclipse.ui.wizards.NodejsProjectWizard");
-		factory.addNewWizardShortcut("org.nodeclipse.ui.wizards.NodejsFileWizard");
-		factory.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");// NON-NLS-1
-		factory.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");// NON-NLS-1
-	}
+    private void addNewWizardShortcuts() {
+        factory.addNewWizardShortcut("org.nodeclipse.ui.wizards.NodejsProjectWizard");
+        factory.addNewWizardShortcut("org.nodeclipse.ui.wizards.NodejsFileWizard");
+        factory.addNewWizardShortcut("org.eclipse.ui.wizards.new.folder");// NON-NLS-1
+        factory.addNewWizardShortcut("org.eclipse.ui.wizards.new.file");// NON-NLS-1
+    }
 
-	private void addViewShortcuts() {
-		factory.addShowViewShortcut("org.eclipse.team.ui.GenericHistoryView"); // NON-NLS-1
-		factory.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
-		factory.addShowViewShortcut(JavaUI.ID_PACKAGES);
-		factory.addShowViewShortcut("org.eclipse.ui.views.ResourceNavigator");
-		factory.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
-		factory.addShowViewShortcut(IPageLayout.ID_OUTLINE);
-	}
+    private void addViewShortcuts() {
+        factory.addShowViewShortcut("org.eclipse.team.ui.GenericHistoryView"); // NON-NLS-1
+        factory.addShowViewShortcut(IConsoleConstants.ID_CONSOLE_VIEW);
+        factory.addShowViewShortcut(JavaUI.ID_PACKAGES);
+        factory.addShowViewShortcut("org.eclipse.ui.views.ResourceNavigator");
+        factory.addShowViewShortcut(IPageLayout.ID_PROBLEM_VIEW);
+        factory.addShowViewShortcut(IPageLayout.ID_OUTLINE);
+    }
 
 }
