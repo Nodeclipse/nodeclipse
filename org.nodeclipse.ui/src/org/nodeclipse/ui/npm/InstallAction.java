86461475a5988d6093d3e17faed174adb99e970b
lamb <lamb52gao@gmail.com>
2012-05-05 23:36:38 +0800

    org.nodeclipse.ui.npm.menu

---
+++ org.nodeclipse.ui/src/org/nodeclipse/ui/npm/InstallAction.java
@@ -0,0 +1,42 @@
+package org.nodeclipse.ui.npm;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IObjectActionDelegate;
+import org.eclipse.ui.IWorkbenchPart;
+import org.nodeclipse.ui.util.LogUtil;
+
+public class InstallAction implements IObjectActionDelegate {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
+     */
+    public void run(IAction action) {
+        System.out.println("npm install");
+        LogUtil.info("npm install");
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action
+     * .IAction, org.eclipse.jface.viewers.ISelection)
+     */
+    public void selectionChanged(IAction action, ISelection selection) {
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * org.eclipse.ui.IObjectActionDelegate#setActivePart(org.eclipse.jface.
+     * action.IAction, org.eclipse.ui.IWorkbenchPart)
+     */
+    public void setActivePart(IAction action, IWorkbenchPart targetPart) {
+    }
+
+}
