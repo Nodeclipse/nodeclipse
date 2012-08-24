804cefc8227c47d34db709021e7f1e1429097f88
lamb <lamb52gao@gmail.com>
2012-08-24 15:02:08 +0800

    window默认是C:/Program Files/nodejs/node.exe *nix下默认是/usr/local/bin/node

--- org.nodeclipse.ui/src/org/nodeclipse/ui/preferences/PreferenceInitializer.java
+++ org.nodeclipse.ui/src/org/nodeclipse/ui/preferences/PreferenceInitializer.java
@@ -3,6 +3,7 @@ package org.nodeclipse.ui.preferences;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.nodeclipse.ui.Activator;
+import org.nodeclipse.ui.util.OSUtils;
 
 /**
  * 
@@ -11,16 +12,17 @@ import org.nodeclipse.ui.Activator;
  */
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
 
-	public PreferenceInitializer() {
-	}
+    public PreferenceInitializer() {
+    }
 
-	@Override
-	public void initializeDefaultPreferences() {
-		IPreferenceStore store = Activator.getDefault().getPreferenceStore();
-		//TODO 这个地方应该像Android那样主动提示设置哇...
-		//TODO 初始化设置这个很坑爹的说，一开始的用户根本不知道需要设置这个，所以肯定是运行出现错误的
-		//window默认是C:/Program Files/nodejs/node.exe *nix下默认是/usr/local/bin/node
-		store.setDefault(PreferenceConstants.P_NODE_PATH, "node");
-	}
+    @Override
+    public void initializeDefaultPreferences() {
+        IPreferenceStore store = Activator.getDefault().getPreferenceStore();
+        String path = "/usr/local/bin/node";
+        if (OSUtils.isWindows()) {
+            path = "C:/Program Files/nodejs/node.exe";
+        }
+        store.setDefault(PreferenceConstants.P_NODE_PATH, path);
+    }
 
 }
