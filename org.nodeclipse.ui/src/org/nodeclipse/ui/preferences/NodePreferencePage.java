c4c33ba51c75b60aa36f57806ee13fb2b324e92d
lamb <lamb52gao@gmail.com>
2012-08-24 15:26:02 +0800

    验证这个选择的文件存在

--- org.nodeclipse.ui/src/org/nodeclipse/ui/preferences/NodePreferencePage.java
+++ org.nodeclipse.ui/src/org/nodeclipse/ui/preferences/NodePreferencePage.java
@@ -6,31 +6,30 @@ import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.nodeclipse.ui.Activator;
 
-public class NodePreferencePage extends FieldEditorPreferencePage implements
-		IWorkbenchPreferencePage {
+public class NodePreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
 
-	public NodePreferencePage() {
-		super(GRID);
-		setPreferenceStore(Activator.getDefault().getPreferenceStore());
-		setDescription("Node Settings");
-	}
+    private FileFieldEditor fileFieldEditor;
+    
+    public NodePreferencePage() {
+        super(GRID);
+        setPreferenceStore(Activator.getDefault().getPreferenceStore());
+        setDescription("Node Settings");
+    }
 
-	@Override
-	public void init(IWorkbench workbench) {
+    @Override
+    public void init(IWorkbench workbench) {
 
-	}
+    }
 
-	@Override
-	protected void createFieldEditors() {
-		addField(new FileFieldEditor(PreferenceConstants.P_NODE_PATH,
-				"Node Path:", getFieldEditorParent()));
-	}
+    @Override
+    protected void createFieldEditors() {
+        fileFieldEditor = new FileFieldEditor(PreferenceConstants.NODE_PATH, "Node Path:", getFieldEditorParent());
+        addField(fileFieldEditor);
+    }
 
-	@Override
-	public boolean isValid() {
-		//window默认是C:/Program Files/nodejs/node.exe *nix下默认是/usr/local/bin/node
-		//这个最好可以检测一些这些是不是存在，如果不存在提示用户安装
-		return super.isValid();
-	}
+    @Override
+    public boolean isValid() {
+        return super.isValid();
+    }
 
 }
